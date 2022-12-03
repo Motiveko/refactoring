7771a9584aa883b68262040dcced8bd26347d7e9
will-ko <will.ko@kakaocorp.com>
2022-12-03 18:54:53 +0900

    적립 포인트 계산 코드 추출

--- ch1/statment.js
+++ ch1/statment.js
@@ -12,11 +12,7 @@ export function statement(invoice, plays) {
     playFor(perf);
     amountFor(perf);
     // 포인트를 적립한다.
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === playFor(perf).type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역 출력
     result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
@@ -26,6 +22,16 @@ export function statement(invoice, plays) {
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === playFor(aPerformance).type) {
+      volumeCredits += Math.floor(aPerformance.audience / 5);
+    }
+    return volumeCredits;
+  }
 
   function playFor(perf) {
     return plays[perf.playID];
