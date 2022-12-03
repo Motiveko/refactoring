152055aaa9633e6270be4935277397fc3e041fc7
Motiveko <rhehdrla@naver.com>
2022-12-03 20:48:20 +0900

    htmlStatement() 추가

--- ch1/statment.js
+++ ch1/statment.js
@@ -4,18 +4,33 @@ function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));  
 }
 
-export {statement};
-
 function renderPlainText(data) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
-    // 청구 내역 출력
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`
+  result += "<table>\n"
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for(let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td>(${perf.audience}석)<td></td>`
+    result += `<td>${usd(perf.amount)}</td></tr>`
+  }
+  result += "</table>\n"
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}점</em></p>\n`;
+
   return result;
 }
 
@@ -26,3 +41,5 @@ function usd(aNumber) {
     minimumFractionDigits: 2,
   }).format(aNumber / 100);
 }
+
+export {statement, htmlStatement};
