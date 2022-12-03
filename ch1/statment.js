bd6398e4935f8172765627d37310a864032035d8
Motiveko <rhehdrla@naver.com>
2022-12-03 20:26:40 +0900

    volumeCredits, totalAmount, totalVolumeCredits 내부함수를 중간 데이터를 사용하도록 변경

--- ch1/statment.js
+++ ch1/statment.js
@@ -3,19 +3,22 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance); // 얕은 복사
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-  
+
   function amountFor(aPerformance) {
     let result = 0;
     switch (aPerformance.play.type) {
@@ -37,6 +40,32 @@ function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === aPerformance.play.type) {
+      volumeCredits += Math.floor(aPerformance.audience / 5);
+    }
+    return volumeCredits;
+  }
+
+  function totalAmount(data) {
+    let totalAmount = 0;
+    for (let perf of data.performances) {
+      totalAmount += perf.amount;
+    }
+    return totalAmount;
+  }
+
+  function totalVolumeCredits(data) {
+    let volumeCredits = 0;
+    for (let perf of data.performances) {
+      volumeCredits += perf.volumeCredits;
+    }
+    return volumeCredits;
+  }
 }
 
 export {statement};
@@ -49,25 +78,9 @@ function renderPlainText(data) {
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function totalAmount() {
-    let totalAmount = 0;
-    for (let perf of data.performances) {
-      totalAmount += perf.amount;
-    }
-    return totalAmount;
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
-  }
 
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
@@ -77,14 +90,5 @@ function renderPlainText(data) {
     }).format(aNumber / 100);
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(aPerformance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === aPerformance.play.type) {
-      volumeCredits += Math.floor(aPerformance.audience / 5);
-    }
-    return volumeCredits;
-  }
 }
 
