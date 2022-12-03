a51bbdc04ced920cfd7936bdd9a205f74cb9fea9
Motiveko <rhehdrla@naver.com>
2022-12-03 20:48:37 +0900

    테스트 snapshot으로 변경 및 htmlStatement 테스트코드 추가

--- ch1/statement.test.js
+++ ch1/statement.test.js
@@ -1,19 +1,15 @@
 import fs from 'fs';
-import {statement} from './statment.js';
+import {htmlStatement, statement} from './statment.js';
 
 const invoicies = JSON.parse(fs.readFileSync('ch1/invoicies.json'));
 const plays = JSON.parse(fs.readFileSync('ch1/plays.json'));
 
-const expectedResult = `청구 내역 (고객명: BigCo)
-Hamlet: $650.00 (55석)
-As You Like it: $580.00 (35석)
-Othello: $500.00 (40석)
-총액: $1,730.00
-적립 포인트: 47점
-`;
 
+const [invoice] = invoicies;
 test('test statement', () => {
-  const [invoice] = invoicies;
-  expect(statement(invoice, plays)).toEqual(expectedResult);
+  expect(statement(invoice, plays)).toMatchSnapshot();
 })
 
+test('test htmlStatement', () => {
+  expect(htmlStatement(invoice, plays)).toMatchSnapshot();
+})
