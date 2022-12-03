1f5ec48f5c6b19335e57c8d942e2cd80c2065983
will-ko <will.ko@kakaocorp.com>
2022-12-03 18:36:59 +0900

    amountFor 함수 추출

--- ch1/statment.js
+++ ch1/statment.js
@@ -10,24 +10,7 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40_000;
-        if (perf.audience > 30) {
-          thisAmount += 1_000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30_000;
-        if (perf.audience > 20) {
-          thisAmount += 10_000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
+    let thisAmount = amountFor(perf, play);
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
@@ -43,4 +26,29 @@ export function statement(invoice, plays) {
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
+
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+    return result;
+  }
 }
+
+
+
